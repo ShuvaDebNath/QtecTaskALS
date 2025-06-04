59651f3583b9ab61a66f6e7c8001fbb8853359c2
Shuva Deb Nath <shuvadebnathbd@gmail.com>
2025-06-05 00:22:11 +0600

    Implement GETJournalEntries with stored procedure

---
+++ QtecTaskALS.Application/JournalEntries/Handlers/GetJournalEntriesQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using QtecTaskALS.Application.JournalEntries.Interfaces;
+using QtecTaskALS.Application.JournalEntries.Queries;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QtecTaskALS.Application.JournalEntries.Handlers;
+
+public class GetJournalEntriesQueryHandler : IRequestHandler<GetJournalEntriesQuery, List<JournalEntryDto>>
+{
+    private readonly IJournalEntryRepository _repo;
+
+    public GetJournalEntriesQueryHandler(IJournalEntryRepository repo)
+    {
+        _repo = repo;
+    }
+
+    public Task<List<JournalEntryDto>> Handle(GetJournalEntriesQuery request, CancellationToken cancellationToken)
+    {
+        var entries = _repo.GetAllJournalEntries();
+        return Task.FromResult(entries);
+    }
+}
---
+++ QtecTaskALS.Application/JournalEntries/Queries/JournalEntryLineDto.cs
@@ -0,0 +1,9 @@
+﻿
+namespace QtecTaskALS.Application.JournalEntries.Queries;
+
+public class JournalEntryLineDto
+{
+    public int AccountId { get; set; }
+    public decimal Debit { get; set; }
+    public decimal Credit { get; set; }
+}
